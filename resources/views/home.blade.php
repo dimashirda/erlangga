7bdaf59fb5d43dbf98970812ebd67b51024754ef
dimashirda <dimas0308@gmail.com>
2018-12-02 11:15:00 +0700

    asdasd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
                 <div class="small-box bg-aqua">
                     <div class="inner">
                         <h3>{{$pembelian->count()}}</h3>
-                        <p>Keuntungan Hari Ini</p>
+                        <p>Keuntungan Hari Ini asdasd</p>
                     </div>
                 </div>
             </div>
