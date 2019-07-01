83c87c2ee02496a9a8c59943106ef6ab07d4b3f6
dimashirda <dimas0308@gmail.com>
2019-07-01 19:43:59 +0700

    update barang import

--- app/Http/Controllers/Barang/ImportController.php
+++ app/Http/Controllers/Barang/ImportController.php
@@ -31,4 +31,32 @@ class ImportController extends Controller
         fclose($file);
         dd('done');
     }
+    public function inputHarga()
+    {
+        DB::beginTransaction();
+        try {
+            $file = fopen("stock.csv","r");
+            $data = fgetcsv($file);
+            $nama_kosong = [];
+            while(!feof($file))
+            {
+                $data = fgetcsv($file);
+                $barang = Barang::where('nama',$data[1])->first();
+                if (empty($barang)) {
+                    array_push($nama_kosong,$data[1]);
+                }
+                else
+                {
+                    $detail = new BarangDetail;
+                    $detail->barang_id = $barang->id;
+                    $detail->jumlah = $data[4];
+                    $detail->harga_beli = $data[3];
+                    $detail->save();    
+                }
+            }
+        } catch (Exception $e) {
+            DB::rollBack();
+            dd($e);
+        }
+    }
 }
